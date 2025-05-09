d31f9c6b01e0ed436c9d093949058251140defb2
Sompong JongmEEsUK <j_sompong@live.com>
2025-05-09 16:21:08 +0700

    9 May 2025
    SIM900A "RING" simu. (VS2022)

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Sim900A_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SerialPort = new System.IO.Ports.SerialPort(this.components);
+            this.SentBox = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // SerialPort
+            // 
+            this.SerialPort.PortName = "COM6";
+            // 
+            // SentBox
+            // 
+            this.SentBox.AllowDrop = true;
+            this.SentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SentBox.Location = new System.Drawing.Point(407, 25);
+            this.SentBox.Multiline = true;
+            this.SentBox.Name = "SentBox";
+            this.SentBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.SentBox.Size = new System.Drawing.Size(347, 184);
+            this.SentBox.TabIndex = 0;
+            this.SentBox.TextChanged += new System.EventHandler(this.SentBox_TextChanged);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 2000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SentBox);
+            this.Name = "Form1";
+            this.Text = "SIm900A Simu. COM6 Tx";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort SerialPort;
+        private System.Windows.Forms.TextBox SentBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Sim900A_2
+{
+    public partial class Form1 : Form
+    {
+        
+        //SerialPort mySerialPort = new SerialPort( );
+        SerialPort mySerialPort = new SerialPort("COM6",9600);
+        //mySerialPort.BaudRate(9600);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_UnLoad(object sender, EventArgs e)
+        {
+           timer.Enabled = false;
+           mySerialPort.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           string message = ((char)13).ToString();
+            byte[] command = new byte[] { 0x13, 0x10 };
+            try
+            {
+                mySerialPort.Open();
+                mySerialPort.Write("RING\r\n");
+                // mySerialPort.WriteLine(message);
+              // mySerialPort.Write(new byte[] { 13 }, 0, 1);
+               // mySerialPort.Write(command);
+                timer.Enabled=true;             
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //SentBox.Text = ex.ToString();
+                //Console.WriteLine(ex);
+            }
+        }
+
+        private void SentBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+             mySerialPort.Write("RING\r\n");
+        }
+    }
+}
